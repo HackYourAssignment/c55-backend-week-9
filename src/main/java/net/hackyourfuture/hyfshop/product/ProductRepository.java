f53a03277ff8ba5d81aed035026def567ad68be3
Salem Ba-Rabuod <barboud@gmail.com>
2026-06-30 19:12:59 +0200

    added product with details

--- src/main/java/net/hackyourfuture/hyfshop/product/ProductRepository.java
+++ src/main/java/net/hackyourfuture/hyfshop/product/ProductRepository.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
 public class ProductRepository {
     private final JdbcClient jdbcClient;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static final RowMapper<Product> PRODUCT_ROW_MAPPER = (rs, _) -> {
         var product = new Product();
@@ -18,12 +23,21 @@ public class ProductRepository {
         product.setPrice(rs.getBigDecimal("price"));
         product.setCategory(rs.getString("category"));
         product.setImageUrl(rs.getString("image_url"));
+        try {
+            String json = rs.getString("details");
+            if (json != null) {
+                product.setDetails(objectMapper.readValue(json,
+                        new TypeReference<Map<String, Object>>() {}));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return product;
     };
 
     public List<Product> getAllProducts() {
         return jdbcClient
-                .sql("SELECT id, title, price, category, image_url FROM products")
+                .sql("SELECT * FROM products")
                 .query(PRODUCT_ROW_MAPPER)
                 .list();
 
