c47a1d4ddeb95a517eb06c397746ee0b30eb11b3
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-07-01 12:17:56 +0200

    Add to the GET /products request the details field from the DB

--- src/main/java/net/hackyourfuture/hyfshop/product/ProductRepository.java
+++ src/main/java/net/hackyourfuture/hyfshop/product/ProductRepository.java
@@ -1,29 +1,45 @@
 package net.hackyourfuture.hyfshop.product;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @AllArgsConstructor
 public class ProductRepository {
     private final JdbcClient jdbcClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static final RowMapper<Product> PRODUCT_ROW_MAPPER = (rs, _) -> {
+    public final RowMapper<Product> PRODUCT_ROW_MAPPER = (rs, _) -> {
         var product = new Product();
         product.setId(rs.getInt("id"));
         product.setTitle(rs.getString("title"));
         product.setPrice(rs.getBigDecimal("price"));
         product.setCategory(rs.getString("category"));
         product.setImageUrl(rs.getString("image_url"));
+
+        try{
+          String json = rs.getString("details");
+          if(json != null){
+              product.setDetails(objectMapper.readValue(json,
+                      new TypeReference<Map<String, Object>>() {
+                      }));
+          }
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+
         return product;
     };
 
     public List<Product> getAllProducts() {
         return jdbcClient
-                .sql("SELECT id, title, price, category, image_url FROM products")
+                .sql("SELECT * FROM products")
                 .query(PRODUCT_ROW_MAPPER)
                 .list();
 
